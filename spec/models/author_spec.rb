a5f5234113a02fb9265a6ef1d52dfbc3ab442677
florian.papsdorf <florian.papsdorf@student.hpi.de>
2020-11-08 14:32:11 +0100

    Extend failing test for #3

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -3,12 +3,13 @@ require 'rails_helper'
 describe "Author model", type: :model do
 
   it "should have correct fields" do
-    author = Author.new("Donald", "Duck", "www.donald-duck.com")
+    author = Author.new({:first_name => "Donald", :last_name => "Duck", :homepage => "www.donald-duck.com"})
     expect(author.first_name).to eq("Donald")
     expect(author.last_name).to eq("Duck")
     expect(author.homepage).to eq("www.donald-duck.com")
     expect(author.first_name).to be_instance_of(String)
     expect(author.last_name).to be_instance_of(String)
     expect(author.homepage).to be_instance_of(String)
+    expect(author.full_name).to eq("Donald Duck")
   end
 end

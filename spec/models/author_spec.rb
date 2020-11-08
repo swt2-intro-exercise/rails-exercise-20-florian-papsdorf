c2b877679c75b4c579d3f04ee585a01c134fc309
florian.papsdorf <florian.papsdorf@student.hpi.de>
2020-11-08 14:36:01 +0100

    Failing test for #4

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -2,14 +2,18 @@ require 'rails_helper'
 
 describe "Author model", type: :model do
 
+  author = Author.new({:first_name => "Donald", :last_name => "Duck", :homepage => "www.donald-duck.com"})
+
   it "should have correct fields" do
-    author = Author.new({:first_name => "Donald", :last_name => "Duck", :homepage => "www.donald-duck.com"})
     expect(author.first_name).to eq("Donald")
     expect(author.last_name).to eq("Duck")
     expect(author.homepage).to eq("www.donald-duck.com")
     expect(author.first_name).to be_instance_of(String)
     expect(author.last_name).to be_instance_of(String)
     expect(author.homepage).to be_instance_of(String)
-    expect(author.full_name).to eq("Donald Duck")
+  end
+
+  it "should have a method name which returns the full name of an author" do
+    expect(author.name).to eq("Donald Duck")
   end
 end

ba0fe9253665653e207e1f9917411f667319678a
florian.papsdorf <florian.papsdorf@student.hpi.de>
2020-11-08 16:57:21 +0100

    Failing tests for #5

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -1,17 +1,26 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-describe "New author page", type: :feature do
+describe 'New author page', type: :feature do
+  it 'should render without error' do
+    visit new_author_path
+  end
 
- it "should render withour error" do
-   visit new_author_path
- end
+  it "should have text inputs for an author's first name, last name, and homepage" do
+    visit new_author_path
 
- it "should have text inputs for an author's first name, last name, and homepage" do
-   visit new_author_path
+    # these are the standard names given to inputs by the form builder
+    expect(page).to have_field('author[first_name]')
+    expect(page).to have_field('author[last_name]')
+    expect(page).to have_field('author[homepage]')
+  end
 
-   # these are the standard names given to inputs by the form builder
-   expect(page).to have_field('author[first_name]')
-   expect(page).to have_field('author[last_name]')
-   expect(page).to have_field('author[homepage]')
- end
+  it 'should save given information to database' do
+    visit new_author_path
+    page.fill_in 'author[first_name]', with: 'Donald'
+    page.fill_in 'author[last_name]', with: 'Duck'
+    page.fill_in 'author[homepage]', with: 'www.donald-duck.de'
+    find('input[type="submit"]').click
+  end
 end

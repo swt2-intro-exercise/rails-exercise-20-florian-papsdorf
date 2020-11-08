d8da8732c0417208da744d42aa0625f17c12cb6c
florian.papsdorf <florian.papsdorf@student.hpi.de>
2020-11-08 16:57:33 +0100

    Work in progress

--- app/controllers/author_controller.rb
+++ app/controllers/author_controller.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
 class AuthorController < ApplicationController
-  def index; end
-
   def new; end
+
+  def create
+    @author = Author.new(author_params)
+    @author.save
+    redirect_to root_path, notice: 'Success!'
+  end
+
+  private
+
+  def author_params
+    params.require(:authors).permit(:first_name, :last_name, :homepage)
+  end
 end

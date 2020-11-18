4f4ee0b414c74ec52e084ad593c08210bbe4e888
florian.papsdorf <florian.papsdorf@student.hpi.de>
2020-11-18 14:25:48 +0100

    Failing test for #17

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,4 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
+require 'date'
 
 RSpec.describe Paper, type: :model do
+  it 'should not validate without title' do
+    some_date = DateTime.new(2001, 2, 3, 4, 5, 6)
+    wrong_paper = Paper.new({ venue: 'Leipzig', year: 2020, created_at: some_date, updated_at: some_date })
+    expect(wrong_paper).to_not be_valid
+  end
 end

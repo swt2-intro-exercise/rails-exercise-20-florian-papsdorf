fa2cb5685f768618e0c16178a4c742f263cd01d6
florian.papsdorf <florian.papsdorf@student.hpi.de>
2020-11-18 14:01:49 +0100

    Failing tests for #15

--- spec/features/authors/authors_index_spec.rb
+++ spec/features/authors/authors_index_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 describe 'Show authors index page', type: :feature do
@@ -13,17 +15,24 @@ describe 'Show authors index page', type: :feature do
 
   it 'should display correct information of authors' do
     @alan = FactoryBot.create :author
-    FactoryBot.create :author
     visit authors_path
     expect(page).to have_text(@alan.name)
     expect(page).to have_text(@alan.homepage)
     expect(page).to have_link 'Show', href: author_path(@alan)
     expect(page).to have_link 'Edit', href: edit_author_path(@alan)
+    expect(page).to have_link 'Delete', href: author_path(@alan)
+  end
+
+  it 'should delete author after clicking delete link' do
+    @alan = FactoryBot.create :author
+    count_before = Author.count
+    visit authors_path
+    click_link('Delete')
+    expect(Author.count).to be == count_before - 1
   end
 
   it 'should display a link to add new authors' do
     visit authors_path
     expect(page).to have_link 'New', href: new_author_path
   end
-
 end

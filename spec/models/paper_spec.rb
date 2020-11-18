d2d22d63ee87fcd4bbbf27ef85e3af85aa15721f
florian.papsdorf <florian.papsdorf@student.hpi.de>
2020-11-18 14:31:47 +0100

    Failing test for #19

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -15,4 +15,10 @@ RSpec.describe Paper, type: :model do
     wrong_paper = Paper.new({ title: 'James Bond', year: 2020, created_at: some_date, updated_at: some_date })
     expect(wrong_paper).to_not be_valid
   end
+
+  it 'should not validate without year' do
+    some_date = DateTime.new(2001, 2, 3, 4, 5, 6)
+    wrong_paper = Paper.new({ title: 'James Bond', venue: "Leipzig", created_at: some_date, updated_at: some_date })
+    expect(wrong_paper).to_not be_valid
+  end
 end

6b14d973987681b5827c4f6d4394529ca60e264b
florian.papsdorf <florian.papsdorf@student.hpi.de>
2020-11-18 13:18:26 +0100

    Fix #12

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,7 +8,7 @@ class AuthorsController < ApplicationController
   def create
     @author = Author.new(params.require(:author).permit(:first_name, :last_name, :homepage))
     if @author.save
-      redirect_to root_path, notice: 'Success!'
+      redirect_to @author
     else
       render 'new'
     end
